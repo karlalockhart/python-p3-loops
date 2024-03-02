9d851793063de9725bc9bc5250b0b901f384b8e5
Karla Lockhart <kblockhart@gmail.com>
2024-03-02 11:09:29 -0800

    passed tests

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,37 @@
 #!/usr/bin/env python3
 
-def happy_new_year():
+#def happy_new_year():
     # code goes here!
-    pass
+#    pass
+
+#def square_integers(int_list):
+    # code goes here!
+#    pass
+
+#def fizzbuzz():
+    # code goes here!
+#    pass
+
+#!/usr/bin/env python3
+
+def happy_new_year():
+    countdown = 10
+    while countdown in range(10, 0, -1):
+        print(countdown)
+        countdown -= 1
+    
+    print("Happy New Year!")
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    return [i ** 2 for i in int_list]
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for i in range(1, 101):
+        if not i % 15:
+            print("FizzBuzz")
+        elif not i % 5:
+            print("Buzz")
+        elif not i % 3:
+            print("Fizz")
+        else:
+            print(i)
